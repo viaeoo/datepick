9b2ffb2ad8d5b462640ffcc5d95c33c10c070cfc
viaeoo <viaeoo@gmail.com>
2020-05-20 16:30:48 +0900

    Fix. eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -71,5 +71,6 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
   }
 }
